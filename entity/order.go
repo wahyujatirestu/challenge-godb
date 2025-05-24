d123b10cfd23a9ff8f3e8d1d6673e496636a6682
Restu Adi Wahyujati <adijati1029@gmail.com>
2025-05-25 01:13:34 +0700

    done

--- entity/order.go
+++ entity/order.go
@@ -1,1 +1,20 @@
 package entity
+
+import "time"
+
+type Order struct {
+	OrderId			int
+	CustomerId		int
+	OrderDate		time.Time
+	CompletionDate 	*time.Time
+	ReceivedBy	   	string
+	CreatedAt		string
+	UpdatedAt		string
+}
+
+type OrderDetail struct {
+	OrderDetail	int
+	OrderId		int
+	ServiceId	int
+	Qty			int
+}
--- entity/service.go
+++ entity/service.go
@@ -1,1 +1,10 @@
 package entity
+
+type Service struct {
+	Service_Id 	 int
+	Service_Name string
+	Unit 		 string
+	Price		 int
+	CreatedAt	 string
+	UpdatedAt	 string
+}
